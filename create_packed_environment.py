effbbdcb2f91cd982b1891fcfdfebc5d6554d69e
Matteo Cencini <mcencini@aleatico1.imago7.local>
2023-05-15 18:51:43 +0200

    some bugfixes

--- create_packed_environment.py
+++ create_packed_environment.py
@@ -1,24 +1,25 @@
 import os
 
-def run_docker_command(confpath, outpath=None, envname="env"):
+def run_docker_command(confpath, envname="env", outpath=None):
 
     # convert path to absolute path
     confpath = os.path.realpath(confpath)
 
     # get default path (same as config)
     if outpath is None:
-        outpath = os.path.join(*confpath.split(os.path.sep)[:-1])
-    outpath = os.path.realpath(outpath)
+        outpath = os.path.split(confpath)[0]
+    else:
+        outpath = os.path.realpath(outpath)
 
     # put together command
-    cmd = f"docker run -v {confpath}:/input/env.yml {outpath}:/output conda-builder:alpha {envname}"
+    cmd = f"docker run -v {confpath}:/input/environment.yml -v {outpath}:/output conda-builder:alpha {envname}"
 
     # run
     print(f"Running: {cmd}")
     os.system(cmd)
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     import sys
 
     # gather input args
--- src/script.sh
+++ src/script.sh
@@ -9,7 +9,7 @@ conda pack -n env -o /output/${1}/${1}.tar.gz
 # create unpack script
 touch /output/${1}/unpack_${1}.sh
 echo "#!/bin/bash" >> /output/${1}/unpack_${1}.sh
-echo "tar -xvf ${1}.tar.gz" >> /output/unpack_${1}.sh
+echo "tar -xvf ${1}.tar.gz" >> /output/${1}/unpack_${1}.sh
 echo "rm ${1}.tar.gz" >> /output/${1}/unpack_${1}.sh
 echo 'bash -c "bin/activate && conda-unpack"' >> /output/${1}/unpack_${1}.sh
 chmod +x /output/${1}/unpack_${1}.sh
